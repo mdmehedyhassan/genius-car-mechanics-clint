c30525150854214fe04f7939e7278e85f75bd207
unknown <mdmehedyh55@gamil.com>
2022-02-03 20:05:44 +0600

    just config

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
 console.log(process.env);
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: "AIzaSyCz7r2nIKDeW3vh-HtaKr3HmGr_KOVBTQc",
+    authDomain: "fire-own-test.firebaseapp.com",
+    projectId: "fire-own-test",
+    storageBucket: "fire-own-test.appspot.com",
+    messagingSenderId: "877776482800",
+    appId: "1:877776482800:web:4baffe6f0d8bdc81f62d35"
 };
 
 export default firebaseConfig;
